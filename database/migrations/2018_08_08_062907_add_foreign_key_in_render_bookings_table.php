6146af2e832899ee373f47ce4aeef92f1aaed806
sangcao <sangcao1003@gmail.com>
2018-08-15 15:59:51 +0700

    Booking_3

---
+++ database/migrations/2018_08_08_062907_add_foreign_key_in_render_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyInRenderBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('render_bookings', function (Blueprint $table) {
+            $table->integer('salon_id')->unsigned()->index()->change();
+
+            $table->foreign('salon_id')
+                ->references('id')->on('salons')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('render_bookings', function (Blueprint $table) {
+            $table->dropForeign('render_bookings_salon_id_foreign');
+            $table->integer('salon_id')->change();
+        });
+    }
+}
